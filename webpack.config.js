1e4cbb6dcd4b42352a78a5e4ee9ade41fbf168d7
Nathan Harper <nathan@reflexions.co>
2018-10-13 11:37:41 -0400

    v1.2.0

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const commander = require('commander');
 commander
     .version('0.1.0')
     .option('--analyze', 'Display bundle analysis')
+    .option('--watch', 'Watch for changes.')
     .parse(process.argv);
 
 const plugins = [];
@@ -19,11 +20,15 @@ if (commander.analyze) {
 
 module.exports = {
     mode: process.env.NODE_ENV,
-    watch: true,
+    watch: commander.watch,
     entry: './src/index.jsx',
     output: {
         path: path.resolve(__dirname) + '/dist',
         filename: 'multi-react-select.js',
+        library: 'multi-react-select',
+        libraryTarget: 'umd',
+        publicPath: '/dist/',
+        umdNamedDefine: true,
     },
     module: {
         rules: [
@@ -52,9 +57,27 @@ module.exports = {
     resolve: {
         modules: ['node_modules', 'src'],
         extensions: ['.js', '.jsx'],
+        alias: {
+            react: path.resolve(__dirname, './node_modules/react'),
+            'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
+        },
     },
     optimization: {
         minimize: true,
     },
     context: __dirname,
+    externals: {
+        react: {
+            commonjs: 'react',
+            commonjs2: 'react',
+            amd: 'React',
+            root: 'React',
+        },
+        'react-dom': {
+            commonjs: 'react-dom',
+            commonjs2: 'react-dom',
+            amd: 'ReactDOM',
+            root: 'ReactDOM',
+        },
+    },
 };
